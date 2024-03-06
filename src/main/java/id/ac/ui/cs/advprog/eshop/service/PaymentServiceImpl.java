696e832c208906a9d547168f2607ed3759b858af
syifakaffa <syifa.kaffa@ui.ac.id>
2024-03-06 19:19:42 +0700

    [GREEN] Implement PaymentRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -9,6 +9,7 @@ import id.ac.ui.cs.advprog.eshop.repository.OrderRepository;
 import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
 import java.util.Map;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class PaymentServiceImpl implements PaymentService{
@@ -19,20 +20,45 @@ public class PaymentServiceImpl implements PaymentService{
     private OrderRepository orderRepository;
     @Override
     public Payment addPayment(Order order, String method, Map<String, String> paymentData){
+        if(paymentRepository.findById(order.getId()) == null){
+            Payment payment = new Payment(order.getId(), method, order, paymentData);
+            paymentRepository.save(payment);
+            return payment;
+        }
         return null;
     }
 
     @Override
     public Payment setStatus(Payment payment, String status){
-        return null;
-    }
+        if(!status.equals("SUCCESS") && !status.equals("REJECTED")){
+            throw new IllegalArgumentException("Invalid payment status");
+        }
+        Payment paymentX = paymentRepository.findById(payment.getId());
+        if (paymentX != null) {
+            Payment newPayment = new Payment(payment.getId(), payment.getPaymentMethod(), payment.getOrder(), payment.getPaymentData());
+            newPayment.setStatus(status);
+            if(newPayment.getStatus().equals("SUCCESS")){
+                orderRepository.findById(payment.getId()).setStatus("SUCCESS");
+            }
+            else if(newPayment.getStatus().equals("REJECTED")){
+                orderRepository.findById(payment.getId()).setStatus("FAILED");
+            }
+            else{
+                throw new IllegalArgumentException("Invalid payment status");
+            }
+            paymentRepository.save(newPayment);
+            return newPayment;
+        } else {
+            throw new NoSuchElementException();
+        }
+    };
 
     @Override
     public Payment getPayment(String paymentId){
-        return null;
+        return paymentRepository.findById(paymentId);
     }
     @Override
     public List<Payment> getAllPayments(){
-        return null;
+        return paymentRepository.findAll();
     }
 }
